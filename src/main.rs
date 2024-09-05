68e2798c80f4c82323c7dbf1af4c9b8e83b8499c
justkira <shighgamerhg@gmail.com>
2024-09-05 12:37:14 +0300

    chore: multi grid test

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,50 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
         ..Default::default()
     });
 
-    let texture_handle: Handle<Image> = asset_server.load("prototype-cube-atlas.png");
-
-    let map_size = TilemapSize { x: 16, y: 16 };
-    let tilemap_entity = commands.spawn_empty().id();
+    let texture_handle: Handle<Image> = asset_server.load("prototype-square-atlas.png");
+    
+    let map_size = TilemapSize { x: 8, y: 8};
+    let tile_size = TilemapTileSize { x: 16.0, y: 8.0 };
+    let grid_size = TilemapGridSize { x: 16.0, y: 8.0 };
+    let map_type: TilemapType = TilemapType::Isometric(IsoCoordSystem::Diamond);
+    
     let mut tile_storage = TileStorage::empty(map_size);
+    let tilemap_entity = commands.spawn_empty().id();
+    
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+            let tile_entity = commands
+            .spawn(TileBundle {
+                position: tile_pos,
+                tilemap_id: TilemapId(tilemap_entity),
+                ..Default::default()
+            })
+            .id();
+        tile_storage.set(&tile_pos, tile_entity);
+    }
+}
 
+commands.entity(tilemap_entity).insert(TilemapBundle {
+    grid_size,
+    map_type,
+    size: map_size,
+    storage: tile_storage,
+    texture: TilemapTexture::Single(texture_handle),
+    tile_size,
+    render_settings: TilemapRenderSettings {
+            // render_chunk_size: UVec2::new(8, 8),
+            y_sort: true,
+            ..Default::default()
+        },
+        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 1.0),
+        ..Default::default()
+    });
+    
+    let texture_handle2: Handle<Image> = asset_server.load("prototype-cube-atlas.png");
+    let mut tile_storage = TileStorage::empty(map_size);
+    let tilemap_entity = commands.spawn_empty().id();
+    let tile_size: TilemapTileSize = TilemapTileSize { x: 16.0, y: 16.0 };
     for x in 0..map_size.x {
         for y in 0..map_size.y {
             let tile_pos = TilePos { x, y };
@@ -36,6 +74,9 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 .spawn(TileBundle {
                     position: tile_pos,
                     tilemap_id: TilemapId(tilemap_entity),
+                    // visible: TileVisible {
+                    //     0: false,
+                    // },
                     ..Default::default()
                 })
                 .id();
@@ -43,25 +84,22 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
         }
     }
 
-    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
-    let grid_size = TilemapGridSize { x: 16.0, y: 8.0 };
-    let map_type: TilemapType = TilemapType::Isometric(IsoCoordSystem::Diamond);
-
     commands.entity(tilemap_entity).insert(TilemapBundle {
         grid_size,
         map_type,
         size: map_size,
         storage: tile_storage,
-        texture: TilemapTexture::Single(texture_handle),
+        texture: TilemapTexture::Single(texture_handle2),
         tile_size,
         render_settings: TilemapRenderSettings {
             render_chunk_size: UVec2::new(3, 1),
             y_sort: true,
             ..Default::default()
         },
-        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
+        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, -1.0) * Transform::from_xyz(0.0, -4.0, 0.0),
         ..Default::default()
     });
+    
 }
 
 #[derive(Resource)]
